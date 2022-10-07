d0f4d5926a1b50ee356e7dd9e148de566516e25f
KevinH48264 <1kevin.huang@gmail.com>
2022-10-06 21:39:50 -0400

    updated to ambient temp

--- plot_file.py
+++ plot_file.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 
 # adjust file_to_read value if your txt file has a different name
-file_to_read = 'data/10-4-Experiment-complete.txt'
+file_to_read = 'data/10-6-Experiment-complete.txt' # EDIT THIS
 custom_csv_file_name = 'experiment_file.csv'
 
-time_header_idx, temp_header_idx, mass_header_idx = 0, 7, 1
+time_header_idx, mass_header_idx = 0, 1
 
 # read data
 f = open(file_to_read,'r')
 headers = f.readline().split(',')
+
+ambient_temp_idx = headers.index('WaterTemp2') # EDIT THIS
+output_temp_idx = headers.index('WaterTemp4') # EDIT THIS
 
 f.readline() # skip the first line with different timestamp
 data = []
@@ -18,11 +21,11 @@ mass = []
 for row in f:
     row = row.split(',')
     if (len(row) == len(headers) + 1):
-        # timestring = row[time_header_idx]
-        # pt = datetime.strptime(timestring,'%H:%M:%S')
-        # time.append(pt)
+        time_val = row[time_header_idx]
+        if (' -> ' in time_val):
+            time_val = time_val.split(' -> ')[1]
         time.append(row[time_header_idx])
-        temp.append(float(row[temp_header_idx]))
+        temp.append(float(row[output_temp_idx]) - float(row[ambient_temp_idx]))
         mass.append(float(row[mass_header_idx]))
 
 # plot temperature
@@ -30,10 +33,10 @@ fig, ax = plt.subplots()
 ax.plot(time, temp, color = 'red', marker="o", label = 'Temperature Data')
   
 ax.set_xlabel('Time', fontsize = 12)
-ax.set_ylabel('Temperature (C)', color="red", fontsize = 12)
+ax.set_ylabel('Delta Temperature (C)', color="red", fontsize = 12)
 
 # adjust this spacing if x axis labels are too cluttered or sparse
-spacing = 25
+spacing = 200
 plt.xticks(rotation=90)
 visible = ax.xaxis.get_ticklabels()[::spacing]
 for label in ax.xaxis.get_ticklabels():
@@ -45,7 +48,7 @@ ax2=ax.twinx()
 ax2.plot(time, mass, color="blue", marker="o")
 ax2.set_ylabel("Mass (kg)", color="blue", fontsize=14)
 
-plt.title('Time vs Temperature and Mass', fontsize = 16)
+plt.title('Time vs Delta Temperature and Mass', fontsize = 16)
 plt.show()
 
 # save the plot as a file
